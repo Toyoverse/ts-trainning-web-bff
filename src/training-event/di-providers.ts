7725e2b3ae6920d139912151f18db6ac448bb20f
Victor Santos Batista <victorsantos.developer@gmail.com>
2022-09-21 16:19:34 -0300

    Specifying public and private providers for dependency injection among modules

--- src/training-event/di-providers.ts
+++ src/training-event/di-providers.ts
@@ -6,31 +6,32 @@ import { CardTrainingRewardServiceImpl } from './services/impl/card-training-rew
 import { ToyoPersonaTrainingEventServiceImpl } from './services/impl/toyo-persona-training-event.service-impl';
 import { TrainingEventServiceImpl } from './services/impl/training-event.service-impl';
 
-const providers = {
-  CardTrainingRewardMetadataRepository: {
+export const privateProviders = [
+  {
     provide: di.CARD_TRAINING_REWARD_METADATA_REPOSITORY,
     useClass: CardTrainingRewardMetadataRepositoryImpl,
   },
-  CardTrainingRewardService: {
+  {
     provide: di.CARD_TRAINING_REWARD_SERVICE,
     useClass: CardTrainingRewardServiceImpl,
   },
-  ToyoPersonaTrainingEventRepository: {
+  {
     provide: di.TOYO_PERSONA_TRAINING_EVENT_REPOSITORY,
     useClass: ToyoPersonaTrainingEventRepositoryImpl,
   },
-  ToyoPersonaTrainingEventService: {
-    provide: di.TOYO_PERSONA_TRAINING_EVENT_SERVICE,
-    useClass: ToyoPersonaTrainingEventServiceImpl,
-  },
-  TrainingEventService: {
-    provide: di.TRAINING_EVENT_SERVICE,
-    useClass: TrainingEventServiceImpl,
-  },
-  TrainingEventRepository: {
+  {
     provide: di.TRAINING_EVENT_REPOSITORY,
     useClass: TrainingEventRepositoryImpl,
   },
-};
+];
 
-export default providers;
+export const publicProviders = [
+  {
+    provide: di.TOYO_PERSONA_TRAINING_EVENT_SERVICE,
+    useClass: ToyoPersonaTrainingEventServiceImpl,
+  },
+  {
+    provide: di.TRAINING_EVENT_SERVICE,
+    useClass: TrainingEventServiceImpl,
+  },
+];
